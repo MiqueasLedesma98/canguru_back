12644d0e9bf345f64503bb24b1de02994b5ff31a
miqueasledesma98 <Miqueasledesma>
2024-07-20 00:13:23 -0300

    update 20/07/2024

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -27,31 +27,140 @@ module.exports = {
       next(error);
     }
   },
+  // TODO: Terminar controladores para traer las reservas
   getBookingListClient: async (req, res, next) => {
     try {
       const { uid = "" } = req;
       const { page = 1, limit = 10 } = req.query;
-      // const {} = req.body; // Si se quiere filtrar x algo mas adelante;
+
+      const { date, status } = req.body; // Si se quiere filtrar x algo mas adelante;
+
+      const query = {};
+
+      if (date) query.date = date;
+      if (status) query.status = status;
 
       const bookings = await Booking.aggregate([
-        { $match: { user: new Types.ObjectId(uid) } },
+        {
+          $match: {
+            user: new Types.ObjectId(uid),
+            ...query,
+          },
+        },
         { $skip: Number(page - 1) * Number(limit) },
         { $limit: Number(limit) },
         { $sort: { date: -1 } },
+        {
+          $lookup: {
+            from: "services",
+            localField: "service",
+            foreignField: "_id",
+            as: "service_details",
+            pipeline: [
+              {
+                $lookup: {
+                  from: "categories",
+                  localField: "category",
+                  foreignField: "_id",
+                  as: "category_details",
+                  pipeline: [{ $project: { state: 0 } }],
+                },
+              },
+              { $project: { provider: 0, category: 0, durationOptions: 0 } },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "provider",
+            foreignField: "_id",
+            as: "provider_details",
+            pipeline: [
+              {
+                $project: {
+                  password: 0,
+                  status: 0,
+                  role: 0,
+                  google: 0,
+                  providerData: 0,
+                },
+              },
+            ],
+          },
+        },
+        { $project: { service: 0, user: 0, provider: 0 } },
       ]);
-
-      // await Booking.populate([{
-
-      // }]);
 
       return res.send({ msg: "OK", bookings });
     } catch (error) {
       next(error);
     }
   },
-  getBookingListProvider: async (_req, res, next) => {
+  getBookingListProvider: async (req, res, next) => {
     try {
-      return res.send("Hello world");
+      const { uid = "" } = req;
+      const { page = 1, limit = 10 } = req.query;
+
+      const { date, status } = req.body; // Si se quiere filtrar x algo mas adelante;
+
+      const query = {};
+
+      if (date) query.date = date;
+      if (status) query.status = status;
+
+      const bookings = await Booking.aggregate([
+        {
+          $match: {
+            provider: new Types.ObjectId(uid),
+            ...query,
+          },
+        },
+        { $skip: Number(page - 1) * Number(limit) },
+        { $limit: Number(limit) },
+        { $sort: { date: -1 } },
+        {
+          $lookup: {
+            from: "services",
+            localField: "service",
+            foreignField: "_id",
+            as: "service_details",
+            pipeline: [
+              {
+                $lookup: {
+                  from: "categories",
+                  localField: "category",
+                  foreignField: "_id",
+                  as: "category_details",
+                  pipeline: [{ $project: { state: 0 } }],
+                },
+              },
+              { $project: { provider: 0, category: 0, durationOptions: 0 } },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "user",
+            foreignField: "_id",
+            as: "user_details",
+            pipeline: [
+              {
+                $project: {
+                  password: 0,
+                  status: 0,
+                  role: 0,
+                  google: 0,
+                },
+              },
+            ],
+          },
+        },
+        { $project: { service: 0, user: 0, provider: 0 } },
+      ]);
+
+      return res.send({ msg: "OK", bookings });
     } catch (error) {
       next(error);
     }
--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,5 +1,56 @@
+const { Types } = require("mongoose");
+const { Service } = require("../models");
+
 module.exports = {
-  getServices: async (_req, res, _next) => {
-    return res.send("hello world");
+  getServices: async (req, res, next) => {
+    try {
+      const { page, limit } = req.query;
+
+      const { category, provider } = req.body;
+
+      const query = {};
+
+      if (provider) query.provider = new Types.ObjectId(provider);
+      if (category) query.category = new Types.ObjectId(category);
+
+      const services = await Service.aggregate([
+        { $match: { status: true, ...query } },
+        { $skip: (Number(page) - 1) * Number(limit) },
+        { $limit: Number(limit) },
+        {
+          $lookup: {
+            from: "categories",
+            localField: "category",
+            foreignField: "_id",
+            as: "category_details",
+            pipeline: [{ $project: { state: 0 } }],
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "provider",
+            foreignField: "_id",
+            as: "provider_details",
+            pipeline: [
+              {
+                $project: {
+                  password: 0,
+                  status: 0,
+                  role: 0,
+                  google: 0,
+                  providerData: 0,
+                },
+              },
+            ],
+          },
+        },
+        { $project: { category: 0, provider: 0 } },
+      ]);
+
+      return res.send({ msg: "ok", services });
+    } catch (error) {
+      next(error);
+    }
   },
 };
